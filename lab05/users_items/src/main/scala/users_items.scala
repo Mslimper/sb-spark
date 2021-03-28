c42448a8861e51a08e10b00c229fe500845e523e
Mslimper <40774233+Mslimper@users.noreply.github.com>
2021-03-28 21:06:51 +0300

    Update users_items.scala

--- lab05/users_items/src/main/scala/users_items.scala
+++ lab05/users_items/src/main/scala/users_items.scala
@@ -1,63 +1,104 @@
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types._
-import org.apache.spark.sql.Column
+import org.apache.spark.sql.{Column, DataFrame, SparkSession}
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.Path
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql._
 
-object users_items extends App {
+object users_items {
+  def main(args: Array[String]): Unit = {
 
-  lazy val spark: SparkSession = SparkSession.builder().appName("Pykhtin.lab05").getOrCreate()
+    val normString = (colVal: Column) => {
+      lower(regexp_replace(trim(colVal), "[^a-zA-Z0-9\\s]", "_"))
+    }
 
-  import spark.implicits._
+    val dfToMatrix = (df: DataFrame, groupColNm: String, pivotColNm: String,  Prefix: String) => {
+      df.withColumn(pivotColNm, normString(col(pivotColNm)))
+        .withColumn(pivotColNm, concat(lit(Prefix), col(pivotColNm)))
+        .groupBy(groupColNm).pivot(pivotColNm).count()
+    }
 
-  //spark.conf.set("spark.sql.session.timeZone", "UTC")
+    def expandCols(myCols: Set[String], allCols: Set[String]) = {
+      allCols.toList.map(x => x match {
+        case x if myCols.contains(x) => col(x)
+        case _ => lit(null).as(x)
+      })
+    }
 
-  val inputDir: String = spark.conf.get("spark.users_items.input_dir",
-    "/user/mikhail.pykhtin/visits")
+    val spark = SparkSession.builder().appName(name = "Khanin.Lab05").getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+    val confOutputDir = spark.conf.get("spark.users_items.output_dir", "/user/mikhail.pykhtin/users-items")
+    val confInputDir = spark.conf.get("spark.users_items.input_dir", "/user/mikhail.pykhtin/visits")
+    val confUpdate = if (spark.conf.get("spark.users_items.update") == "1") 1 else 0
 
-  val outputDir: String = spark.conf.get("spark.users_items.output_dir",
-    "/user/mikhail.pykhtin/users-items")
+    println(s"Update param is $confUpdate")
+    println(s"confInputDir param is $confInputDir")
+    println(s"confOutputDir param is $confOutputDir")
 
-  val updMode: String = spark.conf.get("spark.users_items.update", "0")
+    val views = spark.read.json(confInputDir + "/view/")
+    val buys = spark.read.json(confInputDir + "/buy/")
 
-  val buyDf: DataFrame = spark.read
-    .option("header", "true")
-    .json(s"${inputDir}/buy")
+    val uidDates = views.select("uid", "date").union(buys.select("uid", "date"))
 
-  val viewDf: DataFrame = spark.read
-    .option("header", "true")
-    .json(s"${inputDir}/view")
+    val maxInputDate= uidDates.agg(max("date")).collect()(0)(0).toString
 
-  val df: DataFrame = List(buyDf, viewDf).reduce(_ union _)
+    val users = uidDates.select("uid").distinct
 
-  val maxDate: String = df.select(max(date_add(to_date('p_date, "yyyyMMdd"), -1)).as("maxDt"))
-    .withColumn("maxDt", date_format('maxDt, "yyyyMMdd"))
-    .collect
-    .mkString(",")
-    .replaceAll("[\\[\\]]","")
+    val buyMatrix = dfToMatrix(buys.withColumn("item_id", normString(col("item_id"))), "uid", "item_id", "buy_")
+    val viewsMatrix = dfToMatrix(views.withColumn("item_id", normString(col("item_id"))), "uid", "item_id", "view_")
 
-  val pivotDfPre: DataFrame =  df
-    .filter('uid.isNotNull)
-    .filter('item_id.isNotNull)
-    .withColumn("item_id_clear", lower(regexp_replace(col("item_id"), "[ ]|[-]", "_")))
-    .withColumn("underline", lit("_"))
-    .withColumn("item4pivot", concat('event_type, 'underline, 'item_id_clear))
-    .cache
-    .groupBy('uid, 'item4pivot).agg(count("*").as("cnt"))
-    .groupBy('uid).pivot('item4pivot).agg(sum('cnt))
+    val fs = if(confOutputDir.contains("file:/")) FileSystem.getLocal(new Configuration()) else FileSystem.get(new Configuration())
+    if (!fs.exists(new Path(confOutputDir)))
+      fs.mkdirs(new Path(confOutputDir))
+    val status = fs.listStatus(new Path(confOutputDir))
 
-  val pivotDf: DataFrame = pivotDfPre.select(
-    pivotDfPre.columns.map{
-      case n =>
-        if (n.startsWith("view_") || n.startsWith("buy_"))
-          coalesce(col(n), lit(0)).as(n)
-        else col(n)
-    }: _*)
+    val input_matrix = users.join(buyMatrix, Seq("uid"), "left").join(viewsMatrix, Seq("uid"), "left").na.fill(0)
 
-  pivotDf.write
-    .format("parquet")
-    .option("path", s"${outputDir}/$maxDate")
-    .mode("overwrite")
-    .save()
+    val digitsMatch = "(\\d{8})".r
+
+    val fileList = status
+      .map(x=> digitsMatch.findFirstIn(x.getPath.toString).getOrElse(None))
+      .filter(_ != None).map(_.toString)
+
+    def maxDate(date1: String, date2: String): String = if (date1 > date2) date1 else date2
+
+
+
+    val maxOutDate = fileList.reduceOption(maxDate).getOrElse("0")
+    println(s"maxInputDate param is $maxInputDate")
+    println(s"MaxOutData param is $maxOutDate")
+
+    if (confUpdate != 1)
+    {
+      println(s"confUpdate != 0 param is used")
+      input_matrix.write.parquet(confOutputDir+"/"+maxInputDate)
+      println(s"Matrix has been written")
+    }
+    else
+    {
+      println(s"confUpdate == 1 param is used")
+      val previousMatrix = spark.read.parquet(confOutputDir+"/"+maxOutDate)
+      val newCols = input_matrix.columns.toSet
+      val prevCols = previousMatrix.columns.toSet
+      val total = newCols ++ prevCols
+
+      val newMatrix = previousMatrix
+        .select(expandCols(prevCols, total):_*)
+        .union(input_matrix.select(expandCols(newCols, total):_*))
+        .na.fill(0)
+
+      val newOutputDir = if (maxOutDate != "0") confOutputDir+"/" + maxDate(maxOutDate, maxInputDate) else confOutputDir+"/"+maxInputDate
+      println(s"MaxOutData param is $newOutputDir")
+      if (fs.exists(new Path(newOutputDir)))
+        fs.delete(new Path(newOutputDir), true)
+
+      newMatrix.write.parquet(newOutputDir)
+
+      println(s"Matrix has been written")
+    }
+    spark.stop()
+
+
+  }
 
 }
