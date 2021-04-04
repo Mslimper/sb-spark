4806ceb929dbf400dadd870963ff582fa6970552
Mslimper <40774233+Mslimper@users.noreply.github.com>
2021-04-04 17:54:31 +0300

    Update data_mart.scala

--- lab03/data_mart/src/main/scala/data_mart.scala
+++ lab03/data_mart/src/main/scala/data_mart.scala
@@ -1,150 +1,94 @@
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
-import java.net.URL
-import org.apache.spark.sql.cassandra._
-import org.apache.spark.sql.functions.when
-import org.apache.spark.sql.functions.broadcast
 
-import scala.util.{Failure, Success, Try}
+import org.apache.spark.sql.cassandra._
+import org.elasticsearch.spark
+
 
 object data_mart extends App {
-  val spark = SparkSession
-    .builder()
-    .appName("Laba03")
-    .master("yarn")
-    .getOrCreate()
 
-  val dfJson:Dataset[Row] = spark.read.json("/labs/laba03/weblogs.json")
-  val dfJsonCount = dfJson.count
+  var spark = SparkSession.builder().appName(name="lab03").getOrCreate()
 
-  val dfHdfs = dfJson.select(col("uid"), explode(col("visits")).alias("visits")).na.drop
+  var jsonLogs = "hdfs:///labs/laba03/weblogs.json"
+  var user = "mikhail_pykhtin"
+  var pass = "9mYHPVhT"
 
-  val dfHdfsCount = dfHdfs.count
-  dfHdfs.explain
-  dfHdfs.printSchema
-  dfHdfs.show(5, 200, true)
-
-  val esOptions = Map("es.nodes" -> "10.0.0.5:9200",
-    "es.batch.write.refresh" -> "false",
-    "es.nodes.wan.only" -> "true",
-    "es.net.http.auth.user" -> "mikhail.pykhtin",
-    "es.net.http.auth.pass" -> "9mYHPVhT")
-
-  val dfElasticsearch = spark.read
-    .format("org.elasticsearch.spark.sql")
-    .options(esOptions)
-    .load("visits*")
-    .na.drop
-    .withColumn("category_shop", concat(lit("shop_"),
-      regexp_replace(lower(col("category")), "\\s|-", "_")))
-    .select(col("uid"),
-      col("event_type"),
-      col("category_shop"),
-      col("item_id"),
-      col("item_price"),
-      col("timestamp"))
-
-  dfElasticsearch.explain
-  dfElasticsearch.printSchema
-  dfElasticsearch.show(1, 100, true)
-
-  val dfElasticsearchPivot = dfElasticsearch.groupBy("uid").pivot("category_shop").count
-  dfElasticsearchPivot.show(5,200, true)
-
+  //Cassandrsa -  csndrDf
   spark.conf.set("spark.cassandra.connection.host", "10.0.0.5")
   spark.conf.set("spark.cassandra.connection.port", "9042")
   spark.conf.set("spark.cassandra.output.consistency.level", "ANY")
   spark.conf.set("spark.cassandra.input.consistency.level", "ONE")
 
-  val tableOpts = Map("table" -> "clients",
-    "keyspace" -> "labdata")
+  var csndrOpt = Map("table" -> "clients", "keyspace" -> "labdata")
 
-  val dfCassandra = spark.read
-    .format("org.apache.spark.sql.cassandra")
-    .options(tableOpts)
-    .load
-    .na.drop
-    .withColumn("age_cat",
-      when(col("age") >= 18 && col("age") <= 24,"18-24")
-        .when(col("age") >= 25 && col("age") <= 34,"25-34")
-        .when(col("age") >= 35 && col("age") <= 44,"35-44")
-        .when(col("age") >= 45 && col("age") <= 54,"45-54")
-        .when(col("age") >= 55,">=55")
-        .otherwise("Unknown"))
-    .filter(col("age_cat")!=="Unknown")
-    .select(col("uid"), col("gender"), col("age_cat"))
-
-  dfCassandra.explain
-  dfCassandra.printSchema
-  dfCassandra.show(5)
-
-  val jdbcUrl = "jdbc:postgresql://10.0.0.5:5432/labdata?user=mikhail_pykhtin&password=9mYHPVhT"
-  val dfPostgresql = spark
+  var csndrDf = spark
     .read
-    .format("jdbc")
-    .option("url", jdbcUrl)
-    .option("dbtable", "domain_cats")
-    .option("driver", "org.postgresql.Driver")
+    .format("org.apache.spark.sql.cassandra")
+    .options(csndrOpt)
     .load
-    .na.drop
-    .withColumn("category_web", concat(lit("web_"),
-      regexp_replace(lower(col("category")), "\\s|-", "_")))
-    .select(col("domain"), col("category_web"))
+    .withColumn("age_cat", when(col("age") <= 24, "18-24")
+      .when(col("age") <= 34, "25-34")
+      .when(col("age") <= 44, "35-44")
+      .when(col("age") <= 54, "45-54")
+      .otherwise(">=55")
+    )
+    .drop("age")
 
-  dfPostgresql.explain
-  dfPostgresql.printSchema
-  dfPostgresql.show(5)
-
-  def decodeFunc = udf(
-    (url_test:String) => {
-      def getDomaiName(url:String) = Try {
-        new URL(java.net.URLDecoder.decode(url, "UTF-8")).getHost
-      }  match {
-        case Success(valid) => {
-          if (url.toString.startsWith("http")) {
-            valid.replaceAll("""^www(.*?)\.""","")
-          }
-          else {"bad"}
-        }
-        case Failure(e) => {
-          if (url.toString.startsWith("http")) {
-            url.replaceAll("""^(?:http?:\/\/)""","").split("/")(0)
-          }
-          else {"bad"}
-        }
-      }
-      getDomaiName(url_test)
-    }
+  //Elastic - es
+  var esOpt = Map(
+    "es.nodes" -> "10.0.0.5:9200",
+    "es.batch.write.refresh" -> "false",
+    "es.nodes.wan.only" -> "true",
+    "es.net.http.auth.user" -> "mikhail.pykhtin",
+    "es.net.http.auth.pass" -> "9mYHPVhT"
   )
 
-  val result = dfHdfs.join(broadcast(dfPostgresql), dfPostgresql("domain") === decodeFunc(dfHdfs("visits.url")), "inner")
-    .filter(decodeFunc(dfHdfs("visits.url"))!=="bad")
-    .select(col("uid"), col("domain"), col("category_web"))
-  result.explain
-  result.printSchema
-  result.show(5,100,true)
+  var esDf = spark.read
+    .format("es")
+    .options(esOpt)
+    .load("visits*")
+    .withColumn("category", concat(lit("shop_"), lower(regexp_replace(col("category"), "-|/s", "_"))))
 
-  val resultPivotCategoryWeb = result.groupBy("uid").pivot("category_web").count
-  resultPivotCategoryWeb.show(5,200,true)
+  var esDfPvt = esDf.groupBy(col("uid").alias("uid_es")).pivot(col("category")).count()
 
-  val dfHdfsDistinct = dfHdfs.select(col("uid")).distinct
+  //json
+  var jsDf = spark.read.json("hdfs:///labs/laba03/weblogs.json")
+    .select(col("uid"), explode(col("visits")("url")).alias("url"))
+    .withColumn("domain", regexp_replace(callUDF("parse_url", col("url"), lit("HOST")), "^www.", ""))
+    .drop("url")
 
-  val resultAll = dfHdfsDistinct
-    .join(broadcast(dfCassandra), Seq("uid"), "left")
-    .join(broadcast(dfElasticsearchPivot), Seq("uid"), "left")
-    .join(broadcast(resultPivotCategoryWeb), Seq("uid"), "left")
-  resultAll.explain
-  resultAll.printSchema
-  resultAll.show(5, 200, true)
+  var jsDfPvt = jsDf.join(pgDf, Seq("domain"), "inner")
+    .groupBy(col("uid").alias("uid_js")).pivot(col("category")).count()
 
-  resultAll.write
+  //PG
+  var pgUrl = s"jdbc:postgresql://10.0.0.5:5432/labdata?user=${user}&password=${pass}"
+
+  var pgDf = spark
+    .read
     .format("jdbc")
-    .option("url", "jdbc:postgresql://10.0.0.5:5432/mikhail_pykhtin?user=mikhail_pykhtin&password=9mYHPVhT")
-    .option("dbtable", "clients")
+    .option("url", pgUrl)
     .option("driver", "org.postgresql.Driver")
-    .option("truncate", true)
-    .mode("overwrite")
-    .save
+    .option("labdata", "domain_cats")
+    .option("query", "select * from public.domain_cats")
+    .load()
+    .withColumn("category", concat(lit("web_"), lower(regexp_replace(col("category"), "-|/s", "_"))))
 
+  var finalDf = csndrDf.join(broadcast(esDfPvt), csndrDf("uid") === esDfPvt("uid_es"), "left")
+    .join(broadcast(jsDfPvt), csndrDf("uid") === jsDfPvt("uid_js"), "left")
+    .drop("uid_es", "uid_js")
+
+  //финальный датафрейм
+  var pgFinalUrl = s"jdbc:postgresql://10.0.0.5:5432/${user}?user=${user}&password=${pass}"
+
+  finalDf.write
+    .format("jdbc")
+    .option("url", pgFinalUrl)
+    .option("dbtable", "clients")
+    .option("user", user)
+    .option("password", pass)
+    .option("driver", "org.postgresql.Driver")
+    .option("truncate", "true")
+    .mode("overwrite")
+    .save()
   spark.close()
 }
